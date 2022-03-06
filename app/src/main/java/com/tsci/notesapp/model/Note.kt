43637e147adb4049051594213a05c4006c6fbc2e
Burak-Tasci <buraaktascii@outlook.com>
2022-03-07 00:06:40 +0300

    turn item data class to item entity

--- app/src/main/java/com/tsci/notesapp/model/Note.kt
+++ app/src/main/java/com/tsci/notesapp/model/Note.kt
@@ -1,9 +1,16 @@
 package com.tsci.notesapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity
 data class Note(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-    val note: String,
-    val date: Date
+    @ColumnInfo(name = "text")
+    val noteText: String,
+    @ColumnInfo(name = "date")
+    val noteDate: Date
 )
