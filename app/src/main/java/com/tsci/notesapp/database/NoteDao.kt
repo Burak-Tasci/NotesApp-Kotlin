d5d01c5b28665fb8d530bfe647d9640061679848
Burak-Tasci <buraaktascii@outlook.com>
2022-03-07 00:16:45 +0300

    update notedao

--- app/src/main/java/com/tsci/notesapp/database/NoteDao.kt
+++ app/src/main/java/com/tsci/notesapp/database/NoteDao.kt
@@ -1,4 +1,25 @@
 package com.tsci.notesapp.database
 
-class NoteDao {
+import android.content.ClipData
+import androidx.room.*
+import com.tsci.notesapp.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM Note WHERE id = :id")
+    fun getNote(id: Int): Flow<Note>
+
+    @Query("SELECT * from Note ORDER BY date DESC")
+    fun getIAllNotes(): Flow<List<Note>>
 }
