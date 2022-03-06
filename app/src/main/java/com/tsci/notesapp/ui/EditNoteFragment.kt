63aa147bba7eaff9f659bdd932820477035b6380
Burak-Tasci <buraaktascii@outlook.com>
2022-03-06 22:39:46 +0300

    add fragments

--- app/src/main/java/com/tsci/notesapp/ui/EditNoteFragment.kt
+++ app/src/main/java/com/tsci/notesapp/ui/EditNoteFragment.kt
@@ -1,5 +1,9 @@
 package com.tsci.notesapp.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.tsci.notesapp.databinding.EditNoteFragmentBinding
 
@@ -11,5 +15,26 @@ class EditNoteFragment : Fragment() {
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = EditNoteFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 }
--- app/src/main/java/com/tsci/notesapp/ui/NotesFragment.kt
+++ app/src/main/java/com/tsci/notesapp/ui/NotesFragment.kt
@@ -38,4 +38,5 @@ class NotesFragment : Fragment() {
 
     }
 
+
 }
