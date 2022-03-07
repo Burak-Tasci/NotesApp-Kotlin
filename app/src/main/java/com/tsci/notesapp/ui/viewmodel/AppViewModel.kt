870fbd7c34766897ee1f669fe08edd13dd5ad67b
Burak-Tasci <buraaktascii@outlook.com>
2022-03-07 21:08:37 +0300

    create viewmodel

--- app/src/main/java/com/tsci/notesapp/ui/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/tsci/notesapp/ui/viewmodel/AppViewModel.kt
@@ -1,7 +1,26 @@
 package com.tsci.notesapp
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.tsci.notesapp.data.NoteDao
+import com.tsci.notesapp.data.Note
 
-class EditNoteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AppViewModel(private val noteDao: NoteDao) : ViewModel() {
+    val allItems: LiveData<List<Note>> = noteDao.getAllNotes().asLiveData()
+
+
+}
+
+
+class AppViewModelFactory(private val noteDao: NoteDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AppViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AppViewModel(noteDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
 }
