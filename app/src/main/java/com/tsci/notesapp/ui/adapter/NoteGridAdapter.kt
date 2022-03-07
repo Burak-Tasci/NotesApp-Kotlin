b501b6419d939e8df189226494c3cfed6f339e42
Burak-Tasci <buraaktascii@outlook.com>
2022-03-08 01:24:55 +0300

    indentation fix

--- app/src/main/java/com/tsci/notesapp/ui/adapter/NoteGridAdapter.kt
+++ app/src/main/java/com/tsci/notesapp/ui/adapter/NoteGridAdapter.kt
@@ -40,6 +40,7 @@ class NoteGridAdapter : ListAdapter<Note,
 
     }
     companion object DiffCallback : DiffUtil.ItemCallback<Note>() {
+
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
             return oldItem == newItem
         }
