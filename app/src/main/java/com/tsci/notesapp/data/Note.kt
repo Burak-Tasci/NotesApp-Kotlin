22026ea86aea419a6249fa040a9fadbba3a6d72f
Burak-Tasci <buraaktascii@outlook.com>
2022-03-09 20:52:51 +0300

    add title column

--- app/src/main/java/com/tsci/notesapp/data/Note.kt
+++ app/src/main/java/com/tsci/notesapp/data/Note.kt
@@ -9,6 +9,8 @@ import java.util.*
 data class Note(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
+    @ColumnInfo(name = "note_title")
+    val noteTitle: String,
     @ColumnInfo(name = "note_text")
     val noteText: String,
     @ColumnInfo(name = "note_date")
