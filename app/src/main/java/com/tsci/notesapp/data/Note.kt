9062ec281ba0c20ba041887754f30736a5ed8b0e
Burak-Tasci <buraaktascii@outlook.com>
2022-03-10 22:29:52 +0300

    add format date method

--- app/src/main/java/com/tsci/notesapp/data/Note.kt
+++ app/src/main/java/com/tsci/notesapp/data/Note.kt
@@ -3,6 +3,7 @@ package com.tsci.notesapp.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
 import java.util.*
 
 @Entity
@@ -15,4 +16,14 @@ data class Note(
     val noteText: String,
     @ColumnInfo(name = "note_date")
     val noteDate: Date
-)
+){
+    fun getFormattedDate(): String{
+        val dateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH)
+        val date = dateFormat.parse(noteDate.toString())
+        val formattedDate = SimpleDateFormat("dd.MM.yyyy").format(date)
+        return formattedDate
+    }
+}
+
+
+
